1fd960e6bdf7cc2691dbea9b72ec3c0789573c88
Alan Di Masi <alan199912@gmail.com>
2021-01-15 12:33:01 -0300

    Final section four - Router

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// import { PagesRoutingModule } from './pages-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graph1Component } from './graph1/graph1.component';
+import { PagesComponent } from './pages.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graph1Component,
+    PagesComponent
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graph1Component,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ]
+})
+export class PagesModule { }
